a04cbb257c7b0db2fa4a9ba0fafc2d15c05021d6
Gilgamesh <ddevnich@ucmerced.edu>
2015-04-25 15:57:10 -0700

    Purcell emacs mods

---
+++ purcell_mods/init-local.el
@@ -0,0 +1,9 @@
+(set-frame-font "DejaVu Sans Mono-13")
+
+;; Invoke the shells
+;; (eshell)
+;; (rename-buffer "~$")
+;; Invoke multi-term
+;; Add zsh shell
+
+(provide 'init-local)
---
+++ purcell_mods/init-preload-local.el
@@ -0,0 +1,6 @@
+(require-package 'epl)
+(require 'epl)
+
+(add-to-list 'load-path "~/ethemes/color-theme-gooseberry-tomorrow")
+
+(provide 'init-preload-local)
