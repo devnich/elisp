e9492e08832d4b9d6c096f3fb0d5965230ef2b40
Gilgamesh <ddevnich@ucmerced.edu>
2015-04-27 21:36:06 -0700

    Annotated init-local.el

--- purcell_mods/init-local.el
+++ purcell_mods/init-local.el
@@ -3,6 +3,19 @@
 ;; Invoke the shells
 (add-hook 'emacs-startup-hook 'eshell)
 
-;; Invoke multi-term and add zsh shell - add a multi-term-init.el
+;; Invoke multi-term and add zsh shell
+
+;; sudo access to directories or files on demand; see:
+;;   http://emacsredux.com/blog/2013/04/21/edit-files-as-root/ option B
+
+;; Find keyboard eval shortcut for scratch buffer
+
+;; Move cursor to most recently opened file in ibuffer
+
+;; Add syntax highlighting for drush make and Drupal PHP files if necessary
+
+;; Add some keyboard shortcuts
+(defalias 'qrr 'query-replace-regexp)
+(global-set-key [f5] 'call-last-kbd-macro)
 
 (provide 'init-local)
